58b2966dc1a09132435688a885674b98a1dc33bd
nurhossainfarid <faahsan1516@gmail.com>
2025-02-22 16:39:01 +0600

    register page and login page done

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,7 +1,15 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 import { ReactNode } from "react";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default CommonLayout;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/modules/auth/register/RegisterForm";
+
+const Login = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Login;
